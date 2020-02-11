3fd47933e1225ec0cf1eb1d33a143071c969f84e
Santiago Rodríguez <santo1996.29@gmail.com>
2020-02-11 14:01:05 -0400

    stock lookup in the views, validations too

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,14 @@
 class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new
-    client.price(ticker_symbol)
+
+    begin
+      company_name = client.company(ticker_symbol).company_name
+      stock_price = client.price(ticker_symbol)
+    rescue => e
+      return nil
+    end
+
+    new(ticker: ticker_symbol, name: company_name, price: stock_price)
   end
 end
