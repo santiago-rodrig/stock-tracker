3dd54d8a1fa58a24c2b2f2d1024ac3e387afdc7c
Santiago Rodríguez <santo1996.29@gmail.com>
2020-02-11 17:38:47 -0400

    adding stocks from app GUI is now possible

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,2 +1,9 @@
 class UserStocksController < ApplicationController
+  def create
+    stock = Stock.find_or_init(params[:ticker])
+    stock.save
+    @user_stock = UserStock.create(user: current_user, stock: stock)
+    flash[:notice] = "Stock #{stock.name} was succesfully added to your portfolio"
+    redirect_to my_portfolio_path
+  end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,6 +3,11 @@ class Stock < ApplicationRecord
   has_many :users, through: :user_stocks
   validates :name, :ticker, presence: true
 
+  after_find -> (stock) do
+    tmp = Stock.new_lookup(stock.ticker)
+    stock.update_attribute(:price, tmp.price) if tmp.price != stock.price
+  end
+
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new
 
@@ -14,5 +19,9 @@ class Stock < ApplicationRecord
     end
 
     new(ticker: ticker_symbol, name: company_name, price: stock_price)
+  end
+
+  def self.find_or_init(ticker_symbol)
+    find_by(ticker: ticker_symbol) || new_lookup(ticker_symbol)
   end
 end
