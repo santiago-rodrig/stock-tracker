8b93139a4aa596612713be91818eaa94c0a5b4f9
Santiago Rodríguez <santo1996.29@gmail.com>
2020-02-12 18:56:05 -0400

    edit user info added devise permitted keys, form

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,10 @@ class User < ApplicationRecord
   def can_track_stock?(ticker_symbol)
     !(stocks.count == 10 || stocks.find_by(ticker: ticker_symbol))
   end
+
+  def full_name
+    return first_name.to_s + ' ' + last_name.to_s if first_name || last_name
+
+    'Anonymous'
+  end
 end
