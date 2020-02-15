65c8e3f3b8e1dd8dfa5b0c92f4175155b3df412f
Santiago Rodríguez <santo1996.29@gmail.com>
2020-02-14 21:04:10 -0400

    bad ordering of routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get 'my_friends', to: 'friendships#my_friends'
-  resources :users, only: :show
   get 'search_friend', to: 'friendships#search'
   resources :friendships, only: [:create, :destroy]
   resources :user_stocks, only: [:create, :destroy]
   devise_for :users
+  resources :users, only: :show
   root 'static_pages#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
